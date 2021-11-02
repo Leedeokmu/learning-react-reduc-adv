47cf57c0867036310e010dbd423de8efc27b3d1a
freeefly <freefly3557@gmail.com>
2021-11-02 22:29:28 +0900

    feat: async(redux-promise) 구현

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -3,11 +3,12 @@ import {Provider} from "react-redux";
 import {applyMiddleware, createStore, compose} from "redux";
 import reducers from "./reducers";
 import reduxPromise from "redux-promise"
+import async from "./middlewares/async";
 const Root = (props) => {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     reducers, props.initialState || {},
-    composeEnhancers(applyMiddleware(reduxPromise))
+    composeEnhancers(applyMiddleware(async))
   );
   return (
     <Provider store={store}>
---
+++ testing/src/middlewares/async.js
@@ -0,0 +1,15 @@
+const async = ({dispatch}) => (next) => (action) => {
+  // check to see if the action has a promise on its 'payload' property
+  // If it doesn't, then send the action on to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+  // If it does, then wait for it to resolve
+  // we want to wait for the promise to resolve and then creat a new action with that data and dispatch it
+  action.payload.then(function (response) {
+    const newAction = {...action, payload: response};
+    dispatch(newAction);
+  });
+}
+
+export default async;
